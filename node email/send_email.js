439b82451f6c74a6b8fa1e1f83736bdebd3e88d4
Vaibhav Bansal <77711189+Vaibhav165@users.noreply.github.com>
2021-11-25 19:20:58 +0530

    Update send_email.js

--- node email/send_email.js
+++ node email/send_email.js
@@ -12,8 +12,7 @@ var mailOptions = {
   from: 'vaibhavbansal729@gmail.com',
   to: 'vaibhav2552001@gmail.com',
   subject: 'Sending Email using Node.js',
-  text: `Hi Smartherd, thank you for your nice Node.js tutorials.
-          I will donate 50$ for this course. Please send me payment options.`
+  text: `Hi sending first email`
   // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'        
 };
 
